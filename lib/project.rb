19b4e5b354b239496d0ae7a435b6c894a7dba8c9
Student <example@example.com>
2019-12-16 10:33:24 -0800

    add test and passing code to find equilateral triangles

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,8 @@ class Triangle
  def checker
    if (@side1 + @side2) <= @side3 || (@side1 + @side3) <= @side2 || (@side3 + @side2) <= @side1
      false
+   elsif @side1 == @side2 && @side2 == @side3
+     "equilateral"
    end
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -7,4 +7,8 @@ describe ('#project') do
     test1 = Triangle.new(2,2,8)
     expect(test1.checker).to(eq(false))
   end
+  it("should find an equilateral triangle if all sides are equal")do
+    test2 = Triangle.new(2,2,2)
+    expect(test2.checker).to(eq("equilateral"))
+  end
 end
